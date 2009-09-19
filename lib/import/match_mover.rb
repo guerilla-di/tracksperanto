3eceed60ed3d858b5e55d25e314a06e9e159329a
Julik <me@julik.nl>
2009-09-19 10:33:36 +0200

    Add experimental MatchMover import

---
+++ lib/import/match_mover.rb
@@ -0,0 +1,61 @@
+class Tracksperanto::Import::MatchMover < Tracksperanto::Import::Base
+  
+  def self.autodetects_size?
+    true
+  end
+  
+  def self.human_name
+    "MatchMover .rz2 file"
+  end
+  
+  def parse(io)
+    trackers = []
+    detect_format(io)
+    extract_trackers(io)
+  end
+  
+  private
+  
+  def detect_format(io)
+    lines = (0..2).map{ io.gets }
+    last_line = lines[-1]
+    w, h, _ = last_line.scan(/(\d+)/).flatten
+    @width, @height = w.to_i, h.to_i
+  end
+  
+  def extract_trackers(io)
+    tracks = []
+    while(line = io.gets) do
+      tracks << extract_track(line, io) if line =~ /^pointTrack/
+    end
+    tracks
+  end
+  
+  def extract_track(start_line, io)
+    tracker_name = start_line.scan(/\"([^\"]+)\"/).to_s
+    t = Tracksperanto::Tracker.new(:name => tracker_name)
+    while(line = io.gets) do
+      return t if line =~ /\}/
+      t.keyframes.push(extract_key(line.strip)) if line =~ /^(\s+?)(\d)/
+    end
+    raise "Track didn't close"
+  end
+  
+  LINE_PATTERN = /(\d+)(\s+)([\-\d\.]+)(\s+)([\-\d\.]+)(\s+)(.+)/
+  
+  def extract_key(line)
+    frame, x, y, residual, rest = line.scan(LINE_PATTERN).flatten.reject{|e| e.strip.empty? }
+    Tracksperanto::Keyframe.new(
+      :frame => (frame.to_i() - 1),
+      :abs_x => x,
+      :abs_y => y,
+      :residual => extract_residual(residual)
+    )
+  end
+  
+  def extract_residual(residual_segment)
+    # Parse to the first opening brace and pick the residual from there
+    float_pat = /([\-\d\.]+)/
+    1 - residual_segment.scan(float_pat).flatten.shift.to_f
+  end
+end
---
+++ test/import/test_match_mover_import.rb
@@ -0,0 +1,44 @@
+require File.dirname(__FILE__) + '/../helper'
+
+class MatchMoverImportTest < Test::Unit::TestCase
+  DELTA = 0.01
+  
+  P = File.dirname(__FILE__) + '/samples/kipPointsMatchmover.rz2'
+  
+  def test_introspects_properly
+    i = Tracksperanto::Import::MatchMover
+    assert_equal "MatchMover .rz2 file", i.human_name
+    assert i.autodetects_size?
+  end
+  
+  def test_parsing_from_matchmover
+    fixture = File.open(P)
+    
+    parser = Tracksperanto::Import::MatchMover.new
+    parser.width = 2048
+    parser.height = 1176
+    
+    trackers = parser.parse(fixture)
+    assert_equal 2560, parser.width
+    assert_equal 1080, parser.height
+    
+    assert_equal 5, trackers.length
+    
+    first_t = trackers[0]
+    assert_equal "Track 01", first_t.name
+    assert_equal 131, first_t.length
+    first_kf = first_t[0]
+    
+    assert_equal 0, first_kf.frame
+    assert_in_delta 649.523, first_t[0].abs_x, DELTA 
+    assert_in_delta 423.929, first_t[0].abs_y, DELTA
+    assert_in_delta 0.2, first_t[0].residual, DELTA
+    
+    last_kf = first_t[-1]
+    assert_equal 130, last_kf.frame
+    assert_in_delta 603.148, last_kf.abs_x, DELTA 
+    assert_in_delta 728.46, last_kf.abs_y, DELTA
+    assert_in_delta 0.2, first_kf.residual, DELTA
+  end
+  
+end

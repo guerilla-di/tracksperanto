830225e18f1856a168e837b2faf00c40a50c24af
Julik <me@julik.nl>
2009-09-21 17:33:49 +0200

    Mod version and release

--- lib/tracksperanto.rb
+++ lib/tracksperanto.rb
@@ -4,7 +4,7 @@ require 'delegate'
 
 module Tracksperanto
   PATH = File.expand_path(File.dirname(__FILE__))
-  VERSION = '1.2.0'
+  VERSION = '1.2.1'
   
   module Import; end
   module Export; end

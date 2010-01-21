9e420d420f9e35e326b0fe3800a8b3bc890c5c06
Julik <me@julik.nl>
2010-01-21 01:42:53 +0100

    Improve command line reporting, tag version

--- lib/tracksperanto.rb
+++ lib/tracksperanto.rb
@@ -4,7 +4,7 @@ require 'tempfile'
 
 module Tracksperanto
   PATH = File.expand_path(File.dirname(__FILE__))
-  VERSION = '1.6.8'
+  VERSION = '1.6.9'
   
   module Import; end
   module Export; end

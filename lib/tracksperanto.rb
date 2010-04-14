5c5db67b7985ffa9235bba3d995228b7a60a0a33
Julik <me@julik.nl>
2010-04-14 18:48:34 +0200

    Final modifications for the CLI interface

--- lib/tracksperanto.rb
+++ lib/tracksperanto.rb
@@ -4,7 +4,7 @@ require 'tempfile'
 
 module Tracksperanto
   PATH = File.expand_path(File.dirname(__FILE__))
-  VERSION = '1.9.2'
+  VERSION = '1.9.3'
   
   module Import; end
   module Export; end

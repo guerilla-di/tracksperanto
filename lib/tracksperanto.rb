23a530817b689c76a91448e78a43666994381efa
Julik <me@julik.nl>
2010-08-11 13:22:13 +0200

    Version bump

--- lib/tracksperanto.rb
+++ lib/tracksperanto.rb
@@ -4,7 +4,7 @@ require 'tempfile'
 
 module Tracksperanto
   PATH = File.expand_path(File.dirname(__FILE__))
-  VERSION = '1.9.7'
+  VERSION = '1.9.8'
   
   module Import; end
   module Export; end

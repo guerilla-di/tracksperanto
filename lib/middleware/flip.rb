25d1927617f46e65864b95a86e24bb2f58f2c79b
Julik <me@julik.nl>
2010-08-11 11:11:10 +0200

    Fix Flip middleware for release

--- lib/middleware/flip.rb
+++ lib/middleware/flip.rb
@@ -1,10 +1,16 @@
-# Flips the comp being exported horizontally
+# Flips the comp being exported horizontally or vertically
 class Tracksperanto::Middleware::Flip < Tracksperanto::Middleware::Base
   
-  attr_accessor :enabled
+  attr_accessor :flip, :flop
+  
+  def start_export(w, h)
+    @w, @h = w, h
+    super
+  end
   
   def export_point(frame, float_x, float_y, float_residual)
-    return super unless @enabled
-    super(frame, (float_x * -1), float_y, float_residual)
+    x = @flip ? (@w - float_x) : float_x
+    y = @flop ? (@h - float_y) : float_y
+    super(frame, x, y, float_residual)
   end
 end
--- test/middleware/test_flip_middleware.rb
+++ test/middleware/test_flip_middleware.rb
@@ -3,18 +3,28 @@ require File.dirname(__FILE__) + '/../helper'
 class ShiftMiddlewareTest < Test::Unit::TestCase
   def test_shift_supports_hash_init
     receiver = flexmock
-    m = Tracksperanto::Middleware::Flip.new(receiver, :enabled => true)
-  end
-  
-  def test_shift_bypasses_methods
-    receiver = flexmock
-    receiver.should_receive(:export_point).once.with(1, 123, 95, 0)
-    receiver.should_receive(:export_point).once.with(1, -123, 95, 0)
     
-    m = Tracksperanto::Middleware::Flip.new(receiver, :enabled => true)
-    m.export_point(1, -123, 95, 0)
-    m.enabled = false
-    m.export_point(1, -123, 95, 0)
+    m = Tracksperanto::Middleware::Flip.new(receiver)
+    assert !m.flip
+    assert !m.flop
+    
+    m = Tracksperanto::Middleware::Flip.new(receiver, :flip => true)
+    assert m.flip
+    
+    m = Tracksperanto::Middleware::Flip.new(receiver, :flop => true)
+    assert m.flop
   end
   
+  def test_export_flip_and_flop
+    receiver = flexmock(:exporter)
+    receiver.should_receive(:start_export).once.with(110, 120)
+    receiver.should_receive(:export_point).once.with(1, 100, 95, 0)
+    receiver.should_receive(:export_point).once.with(1, 10, 95, 0)
+    
+    m = Tracksperanto::Middleware::Flip.new(receiver, :flip => true)
+    m.start_export(110, 120)
+    m.export_point(1, 10, 95, 0)
+    m.flip = false
+    m.export_point(1, 10, 95, 0)
+  end
 end

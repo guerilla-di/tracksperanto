f3244a9fe5c0162ae38decf96a1395b4df4e47ce
Julik <me@julik.nl>
2011-01-27 16:01:06 +0100

    Add Flop middleware to manifest. up version

--- lib/tracksperanto.rb
+++ lib/tracksperanto.rb
@@ -4,7 +4,7 @@ require 'tempfile'
 
 module Tracksperanto
   PATH = File.expand_path(File.dirname(__FILE__))
-  VERSION = '2.0.0'
+  VERSION = '2.0.1'
   
   module Import; end
   module Export; end

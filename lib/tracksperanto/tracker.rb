c701bbaacbf0455f27a5c512b9d8afa407c169fa
Julik <me@julik.nl>
2011-01-30 18:53:34 +0100

    Simplify the Tracker code some

--- lib/tracksperanto/tracker.rb
+++ lib/tracksperanto/tracker.rb
@@ -1,6 +1,7 @@
 # Internal representation of a tracker point with keyframes. A Tracker is an array of Keyframe objects and should act and work like one
 class Tracksperanto::Tracker < DelegateClass(Array)
   include Tracksperanto::Casts
+  include Tracksperanto::BlockInit
   include Comparable
   
   # Contains the name of the tracker
@@ -10,17 +11,14 @@ class Tracksperanto::Tracker < DelegateClass(Array)
   def initialize(object_attribute_hash = {})
     @name = "Tracker"
     __setobj__(Array.new)
-    object_attribute_hash.map { |(k, v)| send("#{k}=", v) }
-    yield(self) if block_given?
+    super
   end
   
   def keyframes=(new_kf_array)
     __setobj__(new_kf_array.dup)
   end
-  
-  def keyframes
-    __getobj__
-  end
+
+  alias_method :keyframes, :__getobj__
   
   # Trackers sort by the position of the first keyframe
   def <=>(other_tracker)

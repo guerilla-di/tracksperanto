b885399a34ad16dfc87a452dc045a9214ddddeb0
Julik <me@julik.nl>
2011-02-09 02:08:13 +0100

    Cosmetic

--- lib/import/shake_text.rb
+++ lib/import/shake_text.rb
@@ -9,19 +9,19 @@ class Tracksperanto::Import::ShakeText < Tracksperanto::Import::Base
     io.each do | line |
       if line =~ /TrackName (.+)/
         send_tracker(@last_tracker) if @last_tracker && @last_tracker.any?
-        @last_tracker = Tracksperanto::Tracker.new{|t| t.name = $1 }
+        @last_tracker = Tracksperanto::Tracker.new(:name => $1)
         # Toss the next following string - header
         io.gets
       else
         keyframe_values = line.split
         next if keyframe_values.length < 4
         
-        @last_tracker.keyframes << Tracksperanto::Keyframe.new do | kf |
-          kf.frame = (keyframe_values[0].to_i - 1)
-          kf.abs_x = keyframe_values[1]
-          kf.abs_y = keyframe_values[2]
-          kf.residual = (1 - keyframe_values[3].to_f)
-        end
+        @last_tracker.keyframe!(
+          :frame => (keyframe_values[0].to_i - 1),
+          :abs_x => keyframe_values[1],
+          :abs_y => keyframe_values[2],
+          :residual => (1 - keyframe_values[3].to_f)
+        )
       end
     end
     

9a1547f544274e75be74f01d2e069f2c8f63a8fa
Julik <me@julik.nl>
2011-03-31 13:28:11 +0200

    Up version

--- lib/tracksperanto.rb
+++ lib/tracksperanto.rb
@@ -5,7 +5,7 @@ require 'tempfile'
 
 module Tracksperanto
   PATH = File.expand_path(File.dirname(__FILE__))
-  VERSION = '2.2.0'
+  VERSION = '2.2.1'
   
   module Import; end
   module Export; end

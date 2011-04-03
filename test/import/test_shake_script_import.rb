0f4c61b1a98d3056219e3d3d9cbaa8d390a1f67d
Julik <me@julik.nl>
2011-04-03 12:06:01 +0200

    Update manifest, do a rename

--- test/import/test_shake_script_import.rb
+++ test/import/test_shake_script_import.rb
@@ -125,7 +125,7 @@ class ShakeScriptImportTest < Test::Unit::TestCase
   end
   
   def test_tracker_supernode_with_curves_having_keyframes_at_negative_offsets
-    fixture = File.open(File.dirname(__FILE__) + "/samples/shake_script/304_sd_060_tracking_matchmove_v001.shk")
+    fixture = File.open(File.dirname(__FILE__) + "/samples/shake_script/designated_global_range_starting_at_negative_frame.shk")
     trackers = Tracksperanto::Import::ShakeScript.new(:io => fixture, :width => 720, :height => 576).to_a
     assert_equal 33, trackers.length
     assert_equal 145, trackers[0].length

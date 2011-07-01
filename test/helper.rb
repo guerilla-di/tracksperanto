42489bfdcba2fb75cc5e8246fe04c8e7938fb369
Julik <me@julik.nl>
2011-07-01 15:03:32 +0200

    Do not require rubygems in tests

--- test/helper.rb
+++ test/helper.rb
@@ -1,6 +1,5 @@
 require File.dirname(__FILE__) + '/../lib/tracksperanto' unless defined?(Tracksperanto)
 require 'test/unit'
-require 'rubygems'
 require 'flexmock'
 require 'flexmock/test_unit'
 require 'fileutils'

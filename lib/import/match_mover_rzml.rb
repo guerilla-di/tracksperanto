beb2eb28f07dbde95f9bcd0e7983a2d525e911a2
Julik <me@julik.nl>
2012-02-03 11:19:38 +0100

    Add MatchMover RZML support

---
+++ lib/import/match_mover_rzml.rb
@@ -0,0 +1,81 @@
+# -*- encoding : utf-8 -*-
+require "rexml/document"
+
+class Tracksperanto::Import::MatchMoverRZML < Tracksperanto::Import::Base
+  
+  def self.autodetects_size?
+    true
+  end
+  
+  def self.human_name
+    "MatchMover RZML .rzml file"
+  end
+  
+  def self.distinct_file_ext
+    ".rzml"
+  end
+  
+  class Listener
+    
+    def initialize(from_parser, tracker_callback)
+      @cb = tracker_callback
+      @path = []
+      @parser = from_parser
+      @trackers = []
+    end
+    
+    def depth
+      @path.length
+    end
+    
+    def tag_start(element_name, attrs)
+      @path << element_name.downcase
+      
+      if element_name.downcase == 'shot'
+        @parser.width = attrs["w"]
+        @parser.height = attrs["h"]
+      end
+      
+      return unless @path.include?("ipln")
+      
+      #  <IPLN img="/home/torsten/some_local_nuke_scripts/render_tmp/md_145_1070_right.####.jpg" b="1" e="71">
+      #		<IFRM>
+      #			<M i="1" k="i" x="626.68" y="488.56" tt="0.8000000119" cf="0">
+      if element_name.downcase == 'm'
+        @trackers.push(Tracksperanto::Tracker.new(:name => attrs["i"]))
+      end
+      
+      if element_name.downcase == "ifrm" && attrs["t"]
+        @frame = attrs["t"].to_i
+      end
+      
+      if element_name.downcase == "m"
+        target_marker = @trackers.find{|e| e.name == attrs["i"] }
+        target_marker.keyframe!(:frame => @frame, :abs_x => attrs["x"], :abs_y => (@parser.height - attrs["y"].to_f), :residual => attrs["tt"])
+      end
+    end
+    
+    def tag_end(element_name)
+      @path.pop
+      if element_name.downcase == "rzml"
+        @trackers.reject!{|e| e.empty? }
+        
+        @trackers.each do | recovered_tracker |
+          @cb.call(recovered_tracker)
+        end
+      end
+    end
+    
+    def in?(path_elems)
+      File.join(@path).include?(path_elems)
+    end
+    
+    def xmldecl(*a); end
+    def text(t); end
+  end
+  
+  def each
+    cb = lambda{|e| yield(e) }
+    REXML::Document.parse_stream(@io, Listener.new(self, cb))
+  end
+end
---
+++ test/import/test_match_mover_rzml_import.rb
@@ -0,0 +1,34 @@
+# -*- encoding : utf-8 -*-
+require File.expand_path(File.dirname(__FILE__)) + '/../helper'
+
+class MatchMoverRZMLImportTest < Test::Unit::TestCase
+  DELTA = 0.1
+  
+  P = File.dirname(__FILE__) + '/samples/match_mover_rzml/md_145_1070_right_t1.rzml'
+
+  def test_introspects_properly
+    i = Tracksperanto::Import::MatchMoverRZML
+    assert_equal "MatchMover RZML .rzml file", i.human_name
+    assert i.autodetects_size?
+  end
+  
+  def test_parsing_from_matchmover
+    fixture = File.open(P)
+    
+    parser = Tracksperanto::Import::MatchMoverRZML.new(:io => fixture)
+    trackers = parser.to_a
+    
+    assert_equal 2048, parser.width
+    assert_equal 1152, parser.height
+    
+    assert_equal 2, trackers.length
+    t = trackers[0]
+    
+    assert_equal 0, trackers[0][0].frame
+    assert_equal 1, trackers[0][1].frame
+    
+    assert_in_delta 626.7, trackers[0][0].abs_x, DELTA
+    assert_in_delta 663.4, trackers[0][0].abs_y, DELTA
+  end
+  
+end

d2dcab004359a8ad2d406edb9fce5fc7a0f7e14f
Julik <me@julik.nl>
2012-02-16 09:57:11 +0100

    Export a Maya ASCII file instead of a MEL script
    Export scripts can grow pretty large and pasting them freezes the Script Editor palette for quite a while. Just loading the scene is a better workflow in this case.

--- lib/export/maya_locators.rb
+++ lib/export/maya_locators.rb
@@ -1,13 +1,20 @@
 # -*- encoding : utf-8 -*-
 # Export each tracker as a moving Maya locator
 class Tracksperanto::Export::MayaLocators < Tracksperanto::Export::Base
+  SCENE_PREAMBLE = [
+    '//Maya ASCII 2011 scene',
+    '//Name: TracksperantoLocators.ma',
+    '//Codeset: UTF-8',
+    'requires maya "1.0";'
+  ].join("\n")
+  
   PREAMBLE = 'polyPlane -name "TracksperantoImagePlane" -width %0.5f -height %0.5f;'
   LOCATOR_PREAMBLE = 'spaceLocator -name "%s" -p 0 0 0;'
   KEYFRAME_TEMPLATE = 'setKeyframe -time %d -value %0.5f "%s.%s";';
   MULTIPLIER = 10.0
   
   def self.desc_and_extension
-    "mayaLocators.mel"
+    "mayaLocators.ma"
   end
   
   def self.human_name
@@ -19,6 +26,7 @@ class Tracksperanto::Export::MayaLocators < Tracksperanto::Export::Base
     # and scale the height to that
     @factor = (1 / w.to_f) * MULTIPLIER
     
+    @io.puts(SCENE_PREAMBLE)
     @io.puts(PREAMBLE % [MULTIPLIER, h * @factor])
     @io.puts('rotate -r -os 90;') # Position it in the XY plane
     @io.puts('move -r %0.5f %0.5f 0;' % [MULTIPLIER / 2.0, h * @factor / 2.0])
--- test/export/test_maya_locators_export.rb
+++ test/export/test_maya_locators_export.rb
@@ -4,14 +4,14 @@ require File.expand_path(File.dirname(__FILE__)) + '/../helper'
 class MayaLocatorsExportTest < Test::Unit::TestCase
   include ParabolicTracks
   
-  P_LOCATORS = File.dirname(__FILE__) + "/samples/ref_MayaLocators.mel"
+  P_LOCATORS = File.dirname(__FILE__) + "/samples/ref_MayaLocators.ma"
   
   def test_export_output_written
     ensure_same_output Tracksperanto::Export::MayaLocators, P_LOCATORS
   end
   
   def test_exporter_meta
-    assert_equal "mayaLocators.mel", Tracksperanto::Export::MayaLocators.desc_and_extension
+    assert_equal "mayaLocators.ma", Tracksperanto::Export::MayaLocators.desc_and_extension
     assert_equal "Maya .mel script that generates locators", Tracksperanto::Export::MayaLocators.human_name
   end
 end

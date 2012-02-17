0470c025961eaa35773abd90bbfa53bcaa5de514
Julik <me@julik.nl>
2012-02-17 17:07:32 +0100

    Small formatting fixes

--- lib/tracksperanto/format_detector.rb
+++ lib/tracksperanto/format_detector.rb
@@ -8,32 +8,32 @@ class Tracksperanto::FormatDetector
     freeze
   end
   
-  private
-    def perform_detection(for_path)
-      return unless (for_path && !for_path.to_s.empty?)
-      ext = File.extname(for_path.downcase)
-      @importer_klass = Tracksperanto.importers.find{ |i| i.distinct_file_ext == ext }
-    end
+  # Tells if an importer has been found for this file
+  def match?
+    !!@importer_klass
+  end
   
-  public
-    
-    # Tells if an importer has been found for this extension
-    def match?
-      !!@importer_klass
-    end
-    
-    # Returns the importer if there is one
-    def importer_klass
-      @importer_klass
-    end
-    
-    # Tells if comp size needs to be provided
-    def auto_size?
-      match? ? importer_klass.autodetects_size? : false
-    end
-    
-    # Returns the human name of the importer
-    def human_importer_name
-      match? ? importer_klass.human_name : "Unknown format"
-    end
+  # Returns the importer if there is one
+  def importer_klass
+    @importer_klass
+  end
+  
+  # Tells if comp size needs to be provided
+  def auto_size?
+    match? ? importer_klass.autodetects_size? : false
+  end
+  
+  # Returns the human name of the importer
+  def human_importer_name
+    match? ? importer_klass.human_name : "Unknown format"
+  end
+  
+  private
+
+  def perform_detection(for_path)
+    return unless (for_path && !for_path.to_s.empty?)
+    ext = File.extname(for_path.downcase)
+    @importer_klass = Tracksperanto.importers.find{ |i| i.distinct_file_ext == ext }
+  end
+  
 end

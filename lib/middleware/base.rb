85dfff28158f254963ac6a3101721dca2f6fa759
Julik <me@julik.nl>
2012-02-27 13:20:39 +0100

    Use Forwardable to define Middleware methods

--- lib/middleware/base.rb
+++ lib/middleware/base.rb
@@ -1,4 +1,6 @@
 # -*- encoding : utf-8 -*-
+require "forwardable"
+
 # The base middleware class works just like a Tracksperanto::Export::Base, but it only wraps another exporting object and does not get registered on it's own
 # as an export format. Middleware can be used to massage the tracks being exported in various interesting ways - like moving the coordinates, clipping the keyframes,
 # scaling the whole export or even reversing the trackers to go backwards
@@ -8,6 +10,9 @@ class Tracksperanto::Middleware::Base
   include Tracksperanto::ConstName
   include Tracksperanto::SimpleExport
   include Tracksperanto::Parameters
+  
+  extend Forwardable
+  def_delegators :@exporter, :start_export, :start_tracker_segment, :end_tracker_segment, :export_point, :end_export
   
   # Used to automatically register your middleware in Tracksperanto.middlewares
   # Normally you wouldn't need to override this
@@ -28,10 +33,4 @@ class Tracksperanto::Middleware::Base
     @exporter = exporter_and_args_for_block_init.shift
     super
   end
-  
-  %w( start_export start_tracker_segment end_tracker_segment
-    export_point end_export).each do | m |
-    define_method(m){|*a| @exporter.send(m, *a)}
-  end
-
 end

ca8a10bf4c0a68f9d082846011359f1b42537042
Julik Tarkhanov <me@julik.nl>
2012-10-26 19:25:56 +0200

    Revert "Autoslip 3DE4 exports to frame 1"
    This reverts commit 2a22127cf98f5f5e4dc0005d1954188219b20a5b.

--- test/export/test_equalizer_export.rb
+++ test/export/test_equalizer_export.rb
@@ -13,17 +13,4 @@ class Equalizer4ExportTest < Test::Unit::TestCase
     assert_equal "3de_v4.txt", Tracksperanto::Export::Equalizer4.desc_and_extension
     assert_equal "3DE v4 point export .txt file", Tracksperanto::Export::Equalizer4.human_name
   end
-  
-  def test_always_exports_first_frame_as_0
-    buf = StringIO.new
-    exporter = Tracksperanto::Export::Equalizer4.new(buf)
-    exporter.start_export(1920, 1080)
-    exporter.start_tracker_segment("First")
-    exporter.export_point(14, 10.4, 10.9, 0)
-    exporter.end_tracker_segment
-    exporter.end_export
-    
-    assert_equal "1\nFirst\n0\n1\n1 10.400000000000000 10.900000000000000\n", buf.string, "The first keyframe should have been moved to 1"
-  end
-  
 end

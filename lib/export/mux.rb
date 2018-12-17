85249a51cdc90e23f54eda388e1ac07c93fb4708
Julik Tarkhanov <me@julik.nl>
2018-12-17 03:40:21 +0100

    Remove Mux splat-arg
    as it causes trouble for Flexmock

--- lib/export/mux.rb
+++ lib/export/mux.rb
@@ -1,12 +1,12 @@
 # Multiplexor. Accepts a number of exporters and replays 
 # the calls to all of them in succession.
 class Tracksperanto::Export::Mux
-  def initialize(*outputs)
-    @outputs = outputs.flatten
+  def initialize(outputs)
+    @outputs = outputs
   end
 
   %w( start_export start_tracker_segment end_tracker_segment
     export_point end_export).each do | m |
-    define_method(m){|*a| @outputs.map{|o| o.send(m, *a)}}
+    define_method(m){|*a| @outputs.map{|o| o.public_send(m, *a)}}
   end
 end
